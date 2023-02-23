47d89edff3887a36fbc317d4551819df76d9ec7d
Sturgeon2962 <tomasturgeon@gmail.com>
2023-02-23 15:10:18 +0000

    Create all pages needed

--- Eco_Food_game/Food_Scanner/urls.py
+++ Eco_Food_game/Food_Scanner/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='Food_Scanner-home'),
     path('about/', views.about, name='Food_Scanner-about'),
+    path('logIn/', views.about, name='Food_Scanner-Login'),
+    path('register/', views.about, name='Food_Scanner-register'),
+    path('leaderboard/', views.about, name='Food_Scanner-leaderboard'),
+    path('profile/', views.about, name='Food_Scanner-profile'),
     ]
--- Eco_Food_game/Food_Scanner/views.py
+++ Eco_Food_game/Food_Scanner/views.py
@@ -3,9 +3,45 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'Food_Scanner/home.html')
+
+    context = {
+        'title': "HomePage"
+    }
+    return render(request, 'Food_Scanner/home.html', context)
+
 
 def about(request):
-    return render (request, 'Food_Scanner/about.html')
+    context = {
+        'title': "HomePage"
+    }
+    return render(request, 'Food_Scanner/about.html', context)
 
+
+def logIn(request):
+    context = {
+        'title': "Log-In Page"
+    }
+    return render(request, 'Food_Scanner/logIn.html', context)
+
+
+def register(request):
+    context = {
+        'title': "Registration Page"
+    }
+    return render(request, 'Food_Scanner/register.html', context)
+
+
+def leaderboard(request):
+    context = {
+        'title': "Leaderboard"
+    }
+    return render(request, 'Food_Scanner/leaderboard.html', context)
+
+
+def profile(request):
+    context = {
+        'title': "Profile Page"
+    }
+    return render(request, 'Food_Scanner/profile.html', context)
