6543987fcba01966324b6f2998a796651611c648
Evan Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2023-02-23 12:45:52 +0000

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,26 +1,27 @@
 import json
 import openfoodfacts
-'''
-product = openfoodfacts.products.get_product("80135463")
-name = product['product']['product_name']
-gen = product['product']['generic_name']
-'''
-
 
 def getProduct(barcode):
     product     =   openfoodfacts.products.get_product(str(barcode))
     name        =   product['product']['product_name']
     cal         =   product['product']['nutriments']['energy-kcal_100g']
     nutriScore  =   product['product']['nutriscore_grade']
-    ecoRating   =   product['product']['ecoscore_data']['grade']
+    ecoRating   =   product['product']['ecoscore_grade']
     processed   =   product['product']['nova_groups_tags']
-    img         =   product['product']['selected_images']['front']['display']['en']
+    try:
+        img         =   product['product']['selected_images']['front']['display']['en']
+    except:
+        img = ""
     lib         =   {
         'name':name,
         'energy':cal,
         'nutriscore':nutriScore,
         'ecoRating':ecoRating,
         'processed':processed,
-        'image':img}
+        'image':img,
+        'error':[]}
+
+    for x in range(0,len(lib)):
+        if list(lib.values())[x] == "" or list(lib.values())[x] == "unknown":
+            lib["error"].append(list(lib)[x] + " not found")
     return lib
-print (getProduct(80135463))
