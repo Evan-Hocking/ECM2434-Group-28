f30f07c4e91461bb19943882e62b265c6422b800
Evan Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2023-02-23 15:45:12 +0000

    Update Eco_Food_gameopenFoodFactsPull.py

--- Eco_Food_game/Food_Scanner/Eco_Food_gameopenFoodFactsPull.py
+++ Eco_Food_game/Food_Scanner/Eco_Food_gameopenFoodFactsPull.py
@@ -1,18 +1,41 @@
 import json
 import openfoodfacts
 
-def getProduct(barcode):
-    product     =   openfoodfacts.products.get_product(str(barcode))
+def is_number(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+def getImage(foodDict):
+    try:
+        img     =   foodDict['product']['selected_images']['front']['display']['en']
+        return img
+    except (KeyError,TypeError):
+        try:
+            images = foodDict['product']['selected_images']['front']['display']
+            img = images[0]
+            return img
+        except:
+            return ""
+def getProduct(barcode=0):
+    if not is_number(barcode):
+        return("Err: Invalid Barcode")
+
+    try:
+        product     =   openfoodfacts.products.get_product(str(barcode))
+    except:
+        return ("Err: Request Error")
+    if product['status']==0:
+        return ("Err: Request Error")
     name        =   product['product']['product_name']
     cal         =   product['product']['nutriments']['energy-kcal_100g']
     nutriScore  =   product['product']['nutriscore_grade']
     ecoRating   =   product['product']['ecoscore_grade']
     processed   =   product['product']['nova_groups_tags']
-    try:
-        img         =   product['product']['selected_images']['front']['display']['en']
-    except:
-        img = ""
+    img = getImage(product)
     lib         =   {
+        'barcode':barcode,
         'name':name,
         'energy':cal,
         'nutriscore':nutriScore,
