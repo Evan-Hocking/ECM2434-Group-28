5762adf4fbc371acf548bf34bda53de71b074ec2
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-02-27 21:12:24 +0000

    Add files via upload

---
+++ ECM2434-Group-28-Leaderboard connected with db/ECM2434-Group-28-Leaderboard connected with db/Eco_Food_game/Food_Scanner/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='Food_Scanner-home'),
+    path('about/', views.about, name='Food_Scanner-about'),
+    path('leaderboard/', views.leaderboard, name='Food_Scanner-leaderboard'),
+    path('item/', views.item, name='Food_Scanner-item'),
+]
