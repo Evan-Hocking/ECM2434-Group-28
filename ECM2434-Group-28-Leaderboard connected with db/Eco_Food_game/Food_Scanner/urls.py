7072a392445cb6fc7e37ceb17d4802ee47b22f2a
Evan Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2023-02-27 21:51:20 +0000

    Delete ECM2434-Group-28-Leaderboard connected with db directory

--- ECM2434-Group-28-Leaderboard connected with db/Eco_Food_game/Food_Scanner/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='Food_Scanner-home'),
-    path('about/', views.about, name='Food_Scanner-about'),
-    path('leaderboard/', views.leaderboard, name='Food_Scanner-leaderboard'),
-    path('item/', views.item, name='Food_Scanner-item'),
-]
