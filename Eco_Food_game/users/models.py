52de13aff91ca8eaf8610ccf9b669f5a92d59c72
Sturgeon2962 <tomasturgeon@gmail.com>
2023-02-28 17:50:24 +0000

    profile page up and running

--- Eco_Food_game/users/models.py
+++ Eco_Food_game/users/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    score = models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.user.username} Profile'
