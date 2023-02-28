20b8827e4fbe4409c50ce41d3639e8f40cb3c4c6
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-02-28 01:10:22 +0000

    Add files via upload

--- Eco_Food_game/Food_Scanner/models.py
+++ Eco_Food_game/Food_Scanner/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -5,7 +6,34 @@ from django.db import models
 
 class Demo(models.Model):
     userName = models.CharField(max_length=15)
-    userScore = models.CharField(max_length=99999)
     userEmail = models.CharField(max_length=32)
     userPw = models.CharField(max_length=32)
     role = models.CharField(max_length=32)
+    userScore = models.IntegerField(verbose_name='Score', default=0)
+
+    def __str__(self):
+        return f'{self.userName, self.userScore, self.rank}'
+
+    class Meta:
+        verbose_name = 'Player List'
+        verbose_name_plural = verbose_name
+
+
+class Rank(models.Model):
+    c_id = models.OneToOneField(Demo, on_delete=models.CASCADE,primary_key=True,)
+    rank = models.IntegerField(verbose_name='rank', validators=[MinValueValidator(1)])
+
+# class Score(models.Model):
+#     player = models.ForeignKey(Demo, on_delete=models.CASCADE)
+#     score = models.IntegerField(verbose_name='Score', default=0,
+#                                 validators=[MaxValueValidator(10000000), MinValueValidator(0)])
+#     rank = models.IntegerField(verbose_name='Rank', validators=[MinValueValidator(1)],default=9999)
+
+#     def __str__(self):
+#         return f'{self.score,self.rank}'
+
+#     class Meta:
+#         verbose_name = 'Score List'
+#         verbose_name_plural = verbose_name
+
+
