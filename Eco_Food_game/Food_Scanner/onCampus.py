72a732bcb374ed620a43bfaf54a597d481f74c01
Evan Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2023-03-01 15:33:13 +0000

    Update onCampus.py

--- Eco_Food_game/Food_Scanner/onCampus.py
+++ Eco_Food_game/Food_Scanner/onCampus.py
@@ -5,6 +5,7 @@ with open('../config.json') as json_config:
     config = json.load(json_config)
 
 def getLocation():
+    """Gets users location from Google geolocation API"""
     URL = 'https://www.googleapis.com/geolocation/v1/geolocate?key='+ config['Geolocation_API_Key']
     r = requests.post(URL)
     data = r.json()
@@ -15,6 +16,8 @@ def getLocation():
     return location
 
 def isOnCampus():
+    """Compares user location to specified campus location
+    return true if with 0.75km"""
     campus = (config['uni_lat'],config['uni_long'])
     loc = getLocation()
     dist = geopy.distance.geodesic(campus,loc).km
