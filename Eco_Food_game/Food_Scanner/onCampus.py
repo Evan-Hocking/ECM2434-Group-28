05cc30b7ad81b0d6ee1552b7684e0f8b2741657d
Evan Hocking <95835168+Evan-Hocking@users.noreply.github.com>
2023-03-12 13:52:03 +0000

    Update onCampus.py

--- Eco_Food_game/Food_Scanner/onCampus.py
+++ Eco_Food_game/Food_Scanner/onCampus.py
@@ -1,12 +1,17 @@
 import json
 import requests
 import geopy.distance
+
+#opens and assigns config file to access settings
 with open('../config.json') as json_config:
     config = json.load(json_config)
 
+    """
+    Gets users location from Google geolocation API
+    Pulls the latitude and longitude data
+    return - tuple of (Latitude, Longitude)
+    """
 def getLocation():
-    """Gets users location from Google geolocation API"""
-
     URL = 'https://www.googleapis.com/geolocation/v1/geolocate?key='+ config['Geolocation_API_Key']
     try:
         r = requests.post(URL)
@@ -25,6 +30,11 @@ def getLocation():
     location = (lat,lng)
     return location
 
+    """
+    Gets campus location from config and device location from getLocation()
+    Uses geopy to calculate difference between the two points in km
+    If less than or equal to 0.75km return True, otherwise return false
+    """
 def isOnCampus():
     """Compares user location to specified campus location
     return true if with 0.75km"""
@@ -34,7 +44,7 @@ def isOnCampus():
         return "Err: campus location not found"
     loc = getLocation()
     dist = geopy.distance.geodesic(campus,loc).km
-    if dist<0.75:
+    if dist<=0.75:
         return True
     else:
         return False
