8afa3119a52529bc0306a8a80426cdfa06a81538
Ph1lK4 <122878468+Ph1lK4@users.noreply.github.com>
2023-03-13 16:34:15 +0000

    Update admin.py

--- Eco_Food_game/users/admin.py
+++ Eco_Food_game/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Profile, History
 
 # Register your models here.
-
+"""
+Register extend database
+"""
 admin.site.register(Profile)
 admin.site.register(History)
