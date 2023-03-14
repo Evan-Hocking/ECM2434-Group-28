c32846d7260729787643b954891f78f8ca8e710d
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-03-14 19:52:40 +0000

    Update models.py
    Comment update

--- Eco_Food_game/Food_Scanner/models.py
+++ Eco_Food_game/Food_Scanner/models.py
@@ -1,49 +1,62 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
 
-"""
-Create the Demo table in the data base with the defined fields of the specified type
-"""
 class Demo(models.Model):
+    """
+    Create the Demo table in the data base with the defined fields of the specified type
+    """
     userName = models.CharField(max_length=15)
     userEmail = models.CharField(max_length=32)
     userPw = models.CharField(max_length=32)
     role = models.CharField(max_length=32)
     userScore = models.IntegerField(verbose_name='Score', default=0)
 
-    #Display the table with the title of "username , score, rank"
-    def __str__(self):
+    
+    def __str__(self) -> str:
+        """
+        Display the table with the title of "username , score, rank"
+        :return: The user's username, score and rank
+            type - str
+        """
         return f'{self.userName, self.userScore, self.rank}'
 
+    
     class Meta:
         verbose_name = 'Player List'
         verbose_name_plural = verbose_name
 
-"""
-Create the Demo table in the data base with the defined fields of the specified type
-"""
+        
 class Rank(models.Model):
+    """
+    Create the Demo table in the data base with the defined fields of the specified type
+    """
     c_id = models.OneToOneField(
         Demo, on_delete=models.CASCADE, primary_key=True,)
     rank = models.IntegerField(verbose_name='rank', validators=[
                                MinValueValidator(1)])
 
-"""
-Create the Demo table in the data base with the defined fields of the specified type
-"""
+
 class Score(models.Model):
+    """
+    Create the Demo table in the data base with the defined fields of the specified type
+    """
     player = models.ForeignKey(Demo, on_delete=models.CASCADE)
     score = models.IntegerField(verbose_name='Score', default=0,
                                 validators=[MaxValueValidator(10000000), MinValueValidator(0)])
     rank = models.IntegerField(verbose_name='Rank', validators=[
                                MinValueValidator(1)], default=9999)
 
-    #Display the table with the title of "username , score, rank"
-    def __str__(self):
-        return f'{self.score,self.rank}'
+    
+    def __str__(self) -> str:
+        """
+        Display the table with the title of "username , score, rank"
+        :return: The 
+            type - str
+        """
+        return f'{self.score, self.rank}'
 
+    
     class Meta:
         verbose_name = 'Score List'
         verbose_name_plural = verbose_name
