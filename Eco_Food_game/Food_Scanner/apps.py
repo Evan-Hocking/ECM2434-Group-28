afec06f9cb49a05c9d0db2199d2831b767b905f0
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-03-14 20:04:27 +0000

    Update apps.py
    Comments update

--- Eco_Food_game/Food_Scanner/apps.py
+++ Eco_Food_game/Food_Scanner/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class FoodScannerConfig(AppConfig):
+    """
+    AppConfig class for the Food_Scanner Django app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Food_Scanner'
