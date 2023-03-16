48cad2f82915baef23e72147c05a64bf655ac675
Evan Hocking <erh226@exeter.ac.uk>
2023-03-16 17:21:46 +0000

    modified tests to use assert

--- Eco_Food_game/Food_Scanner/testCustom.py
+++ Eco_Food_game/Food_Scanner/testCustom.py
@@ -1,71 +1,39 @@
 from openFoodFactsPull import getProduct
 import logging
-logging.basicConfig(level=logging.DEBUG)
+
 def testOpenFoodFacts():
     #tests Null input into getProduct() Function
-    try:
-        logging.debug(getProduct())
-    except:
-        logging.error("Null input Error")
+    assert getProduct(),"OFFP Err: Null input Error"
 
     #tests a text input into getProduct()
-    try:
-        logging.debug(getProduct('hello'))
-    except:
-        logging.error("text input error")
+    assert getProduct("hello"),"OFFP Err: text input error"
 
-    #tests an int number into getProduct()
-    try:
-        logging.debug(getProduct(80135463))
-    except:
-        logging.error("int input error")
+    #tests an int number into getProduct(), validity irrelevant
+    assert getProduct(80135463),"OFFP Err: int input error"
 
-    #tests a string number input to getProduct()
-    try:
-        logging.debug(getProduct("80135463"))
-    except:
-        logging.error("string input error")
+    #tests a string number input to getProduct(), validity irrelevant
+    assert getProduct("80135463"),"OFFP Err: string input error"
 
     #tests a number that is not a valid barcode
-    try:
-        logging.debug(getProduct(123))
-    except:
-        logging.error("non present barcode")
-
+    assert getProduct(123),"OFFP Err: non present barcode"
+    
     #tests if a dictionary is returned
-    if type(getProduct(80135463)) is dict:
-        logging.debug("Correct return type")
-    else:
-        logging.debug(type(getProduct(80135463)))
-        logging.error("return type error")
+    assert type(getProduct(80135463)) is dict,"OFFP Err: return type error"
 
     #tests when ecoscore is not present
-    try:
-        lib = getProduct(3017620422003)
-        logging.debug(lib['ecoRating'])
-    except:
-        logging.error('Ecoscore Error')
+    assert getProduct(3017620422003)['ecoRating'],'OFFP Err: Ecoscore Error'
 
     #tests when non english image is present
-    try:
-        lib = getProduct(3228857001316)
-        logging.debug(lib['image'])
-    except:
-        logging.error('Foreign Img Error')
-
+    assert getProduct(3228857001316)['image'],'OFFP Err: Foreign Img Error'
+    
     #tests when no image is present
-    try:
-        lib = getProduct(7622210713780)
-        logging.debug(lib['image'])
-    except:
-        logging.error('No Img Error')
+    assert  getProduct(7622210713780)['image'],'OFFP Err: No Img Error'
 
     #tests when no nutriscore is present
-    try:
-        lib = getProduct(3033710084913)
-        logging.debug(lib['nutriscore'])
-    except:
-        logging.error('No Nutriscore Error')
+    assert getProduct(3033710084913)['nutriscore'],'OFFP Err: No Nutriscore'
+
+    print("OFFP TEST PASS")
+
 def main():
     testOpenFoodFacts()
 main()
