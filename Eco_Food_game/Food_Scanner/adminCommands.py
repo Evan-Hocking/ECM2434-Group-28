306810dc0a406aff7e96ee4baa72323ef15e4808
Sturgeon2962 <tomasturgeon@gmail.com>
2023-03-18 17:08:02 +0000

    zero Points

--- Eco_Food_game/Food_Scanner/adminCommands.py
+++ Eco_Food_game/Food_Scanner/adminCommands.py
@@ -1,4 +1,5 @@
 import json
+import sqlite3
 
 with open('Eco_Food_game/config.json') as json_config:
     config = json.load(json_config)
@@ -8,4 +9,20 @@ def zeroScore(adminCode, userID):
         print("PERMISSION DENIED: INVALID ADMIN CODE")
         return
     #zero score code
+    try:
+        sqliteConnection = sqlite3.connect('db.sqlite3')
+        cursor = sqliteConnection.cursor()
+        print("Connected to SQLite")
+        sql_update_query = f"Update users_profile set score = 0 where id = {userID}"
+        cursor.execute(sql_update_query)
+        sqliteConnection.commit()
+        print("Record Updated successfully ")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to update sqlite table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
 
