70c0d6ef03ad1d56534a0f29f13eb4e75fb24c5a
Evan Hocking <erh226@exeter.ac.uk>
2023-03-20 14:39:30 +0000

    changed arguments to inputs

--- Eco_Food_game/Food_Scanner/adminCommands.py
+++ Eco_Food_game/Food_Scanner/adminCommands.py
@@ -4,16 +4,17 @@ import sqlite3
 with open('Eco_Food_game/config.json') as json_config:
     config = json.load(json_config)
 
-def zeroScore(adminCode, userID):
+def zeroScore():
+    adminCode = int(input("Enter Pin to access Admin Settings: "))
     if not(adminCode == config["adminCode"]):
         print("PERMISSION DENIED: INVALID ADMIN CODE")
         return
-    #zero score code
+    userID = int(input("Enter Id of user to zero: "))
     try:
         sqliteConnection = sqlite3.connect('db.sqlite3')
         cursor = sqliteConnection.cursor()
         print("Connected to SQLite")
-        sql_update_query = f"Update users_profile set score = 0 where id = {userID}"
+        sql_update_query = f"Update 'users_profile' set score = 0 where id = {userID}"
         cursor.execute(sql_update_query)
         sqliteConnection.commit()
         print("Record Updated successfully ")
@@ -26,3 +27,4 @@ def zeroScore(adminCode, userID):
             sqliteConnection.close()
             print("The SQLite connection is closed")
 
+zeroScore()
