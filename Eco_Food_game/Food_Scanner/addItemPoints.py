727da2c33bc69275b30eb16da89b683fb7d5f0fc
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-03-21 02:39:55 +0000

    Update addItemPoints.py

--- Eco_Food_game/Food_Scanner/addItemPoints.py
+++ Eco_Food_game/Food_Scanner/addItemPoints.py
@@ -5,7 +5,7 @@
 # Author: Ryan Gascoigne-Jones
 #---------------------------------------------------------------------------------------
 import sqlite3
-from .Food_Scanner.users.models import Profile, History
+from users.models import Profile, History
 
 
 def isAdd(fragment) -> bool:
