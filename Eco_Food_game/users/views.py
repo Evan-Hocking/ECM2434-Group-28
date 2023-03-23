884e957c7e866140cf8b6e3bfe9a6b29f49c5b75
TheHaoBoy <92601011+TheHaoBoy@users.noreply.github.com>
2023-03-23 01:41:48 +0000

    Update views.py
    Comments updated

--- Eco_Food_game/users/views.py
+++ Eco_Food_game/users/views.py
@@ -18,9 +18,9 @@ def register(request):
     """
     Pass User registration form to the register webpage based on the register.html template
     :param request: The data about request made to webpage
-        type - obj (HttpRequest)
-    :return: The requested webpage based on the supplied template with context passed to it 
         type - HttpRequest
+    :return: The requested webpage based on the supplied template with context passed to it 
+        type - HttpRequest or HttpResponseRedirect
     """
     # If the method of request is POST
     if request.method == 'POST':
@@ -32,8 +32,10 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(
                 request, f'Your account has been created! You can now log in!')
+            # Redirect to the login form
             return redirect('auth-login')
     else:
+        # Sets to the register form
         form = UserRegisterForm()
 
     # Returns and render the webpage and pass the form into the template
@@ -41,29 +43,31 @@ def register(request):
 
 
 @login_required
-def profile(request):
+def profile(request) -> HttpRequest:
     """
     Pass the profile information to the correct webpage
     You need to be logged in to access webpage otherwise you will be redirected to Food_Scanner-home 
     :param request: The data about request made to webpage
-        type - obj (HttpRequest)
+        type - HttpRequest
     :return: The requested webpage based on the supplied template with context passed to it
         type - HttpRequest
     """
     
+    # Gets the current user profile
     curProfile = Profile.objects.get(user=request.user)
-
 
     # Collects history from database
     history = History.objects.filter(userId_id=curProfile.id).order_by('-date_Added')
-
+    
+    # Gets the users ordered by score
     profiles = Profile.objects.order_by('-score')
-
+    
+    # Checks the achievements of the user profile
     checkAchievements(request)
-
     achievements = Achievements.objects.all()
 
     for x in range(0,len(achievements)):
+        # Checking that achievements and user matches
         if achievements[x].Id_id == request.user.id:
             userAchievements = achievements[x]
             break
@@ -84,9 +88,9 @@ def profileUpdate(request):
     Pass UserUpdateForm to the webpage when requested
     You must be logged in to access the webpage if not you will be redirected to Food_Scanner-home
     :param request: The data about request made to webpage
-        type - obj (HttpRequest)
-    :return: The requested webpage based on the supplied template with context passed to it (Profile)
         type - HttpRequest
+    :return: The requested webpage based on the supplied template with context passed to it (Profile)
+        type - HttpRequest or HttpResponseRedirect
     """
 
     if request.method == 'POST':
@@ -95,7 +99,7 @@ def profileUpdate(request):
         pForm = ProfileUpdateForm(
             request.POST, request.FILES, instance=request.user.profile)
         if uForm.is_valid() and pForm.is_valid():
-            #If the forms are valid save the results
+            # If the forms are valid save the results
             uForm.save()
             pForm.save()
             messages.success(request, f'Your account has been updated!')
@@ -111,7 +115,15 @@ def profileUpdate(request):
     }
     return render(request, 'users/profile_update.html', context)
 
+
 def profile_list(request):
+    """
+    Checks if the user has been authenticated, taking them to the profile list page otherwise redirect them to the login page
+    :param request: The pull request of the user authentication, if they are authenticated or not
+        type - HttpRequest
+    :return: The data request to the profiles page or redirect to the login page
+        type - HttpRequest or HttpResponseRedirect
+    """
     if request.user.is_authenticated:
         profiles = Profile.objects.exclude(user=request.user)
         return render(request, 'users/profile_list.html', {'profiles': profiles})
